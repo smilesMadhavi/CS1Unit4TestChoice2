b7fdfcf1b238f23b26a4257aa0cf2195ec5ff70d
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-11-05 12:23:26 -0600

    Add files via upload

---
+++ Divisors.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class Divisors
+{
+	public static String getDivisors( int number )
+	{
+		String divisors="";
+		return divisors; 
+	}
+}
---
+++ DivisorsRunner.java
@@ -0,0 +1,15 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class DivisorsRunner
+{
+	public static void main( String args[] )
+	{
+              System.out.println( "10 has divisors " +Divisors.getDivisors(10));
+
+		//add test cases 
+	}
+}
